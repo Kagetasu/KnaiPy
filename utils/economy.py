dab16d0b0171ece196acd38ff56d0ad6272c2d0b
Kagetasu <52426531+Kagetasu@users.noreply.github.com>
2023-09-23 18:20:12 +0300

    Added a user check query for `update` and `get_balance`

--- utils/economy.py
+++ utils/economy.py
@@ -2,15 +2,19 @@ import asqlite
 
 from typing import Literal
 
+USER_CHECK = """
+            INSERT INTO ECONOMY (id, balance) VALUES (?, 0)
+            ON CONFLICT(id) DO
+            """
 
 class Database:
-    def __init__(self, database_file="economy.db") -> None:
+    def __init__(self, database_file="./databases/economy.db") -> None:
         self.database_file = database_file
 
     async def create_user(self, user_id: int, bal: int = 0):
         async with asqlite.connect(self.database_file) as conn:
             await conn.execute(
-                "INSERT INTO ECONOMY (ID, BALANCE) VALUES (?, ?);", (user_id, bal)
+                "INSERT INTO ECONOMY (ID, BALANCE) VALUES (? ,?)", (user_id, bal)
             )
             await conn.commit()
 
@@ -22,27 +26,27 @@ class Database:
                     ID INT PRIMARY KEY NOT NULL,
                     BALANCE INT NOT NULL
                 );
-            """
+                """
             )
             await conn.commit()
 
     async def update(
-        self,
-        user_id: int,
-        mode: Literal["set", "+", "-"],
-        amnt: int,
-    ):
+            self,
+            user_id: int,
+            mode: Literal["set", "+", "-"],
+            amnt: int
+    ) -> None:
         async with asqlite.connect(self.database_file) as conn:
             if mode == "set":
-                query = "UPDATE ECONOMY set BALANCE = ? where ID = ?;"
+                query = "UPDATE set BALANCE = ? where ID = ?;"
             elif mode == "+":
-                query = "UPDATE ECONOMY set BALANCE = BALANCE + ? where ID = ?"
+                query = "UPDATE set BALANCE = BALANCE + ? where ID = ?;"
             elif mode == "-":
-                query = "UPDATE ECONOMY set BALANCE = BALANCE - ? where ID = ?"
+                query = "UPDATE set BALANCE = BALANCE - ? where ID = ?;"
             else:
-                raise RuntimeError(f"Expected `set`, `+` or `-` but recieved {mode}")
-
-            await conn.execute(query, (amnt, user_id))
+                raise RuntimeError(f"Expected `set`, `+` or `-` but received {mode}")
+            
+            await conn.execute(USER_CHECK+query, (user_id, amnt, user_id))
 
             await conn.commit()
 
@@ -54,22 +58,15 @@ class Database:
     async def get_balance(self, user_id: int) -> int:
         async with asqlite.connect(self.database_file) as conn:
             query = await conn.execute(
-                """
-            SELECT balance from ECONOMY 
-                WHERE ID = ?
-            """,
-                (user_id,),
+                USER_CHECK +
+                        """
+                        UPDATE SET balance = balance
+                        RETURNING balance
+                    """,
+                (user_id)
             )
-
+            
             user = await query.fetchone()
             return user[0]
-
-    async def check_user(self, user_id: int):
-        async with asqlite.connect(self.database_file) as conn:
-            query = await conn.execute(
-                "SELECT ID from ECONOMY where ID = ?", (user_id,)
-            )
-
-            user = await query.fetchone()
-            if not user:
-                await self.create_user(user_id=user_id)
+    
+    
