146ed018b626fe26a3c564aae0f751710d0dc7d5
Kagetasu <kuesugi33@gmail.com>
2023-09-26 20:06:04 +0300

    .

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import discord
 from discord.ext import commands
 
@@ -26,7 +25,6 @@ async def setup_hook():
     bot.words = Words()
     await bot.words.create_tables()
     
-    
     CMDS_FOLDER = Path('./commands')
     for cmd in CMDS_FOLDER.glob("**/[!_]*.py"):
         await bot.load_extension(cmd.as_posix().removesuffix(".py").replace('/', '.'))
@@ -39,36 +37,3 @@ bot.run(token)
 
 
 
-=======
-import discord
-from discord.ext import commands
-
-from pathlib import Path
-
-from utils.economy import Database
-from config import token
-
-intents = discord.Intents.default()
-intents.message_content = True
-intents.members = True
-
-bot = commands.Bot(
-    command_prefix="`",
-    intents=intents,
-)
-
-
-@bot.event
-async def setup_hook():
-    bot.db = Database()  # type: ignore
-    await bot.db.create_tables()  # type: ignore
-
-    CMDS_FOLDER = Path("./commands")
-    for cmd in CMDS_FOLDER.glob("**/[!_]*.py"):
-        await bot.load_extension(cmd.as_posix().removesuffix(".py").replace("/", "."))
-
-    print(f"Logged in as {bot.user}")
-
-
-bot.run(token)
->>>>>>> 17a1b712a1b761c036931e9ed25ca5d97b4ef372
